589c7afaf9a03c6706745ef4ac8ea93a74b9da50
Liz <eracca@udel.edu>
2016-09-21 14:16:12 -0400

    Lab2

---
+++ Lab2/lab2lottery.cc
@@ -0,0 +1,14 @@
+#include "lab2lottery.hh"
+
+Owner *makeOwner(){
+}
+
+
+
+
+
+Store makeStore();
+Customer makeCustomer();
+void getWinners (int winners[]);
+void findWinners(Owner *owner, int *ls);
+int checkwin(int *nums, int *winners);
