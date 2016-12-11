dbfd48040cdb20229e169bb259cab93a8514974c
Liz <eracca@udel.edu>
2016-12-11 00:43:38 -0500

    final version

--- NewarkMap/Main.cc
+++ NewarkMap/Main.cc
@@ -17,24 +17,16 @@ int main(){
     Map* m = new Map("mapdata.csv"); 
     cout << "read map" << endl; 
 
-    //select node as source node and 3 target nodes to test
+    //select node as source node and a target node to test
     Node* source = m->nodeList->findNode("170139186518"); 
-    Node* t1 = m->nodeList->findNode("171635186792"); 
-    Node* t2 = m->nodeList->findNode("170259185821"); 
-    Node* t3 = m->nodeList->findNode("171699187388"); 
+    Node* t1 = m->nodeList->findNode("170259185821"); 
 
     //run Djikstra on the source
     m->Djikstra(source); 
 
-    //find path to targets, print paths
+    //find path to target, print path
     EdgeLL* p1 = m->findPath(t1);
-    EdgeLL* p2 = m->findPath(t2);
-    EdgeLL* p3 = m->findPath(t3);
-    cout << "path 1:" << endl;  
+    cout << "path:" << endl;  
     p1->printEdges();
-    cout << "path 2:" << endl;  
-    p2->printEdges();
-    cout << "path 3:" << endl;  
-    p3->printEdges();
     return 0; 
 }
